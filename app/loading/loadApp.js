49bc2e04595c1161646ca24b8cf978318bfb55a3
zhanghedong <zhanghedong@gmail.com>
2015-09-23 00:34:30 +0800

    添加Loading代码

---
+++ app/loading/loadApp.js
@@ -0,0 +1,107 @@
+﻿
+var gui = require('nw.gui');
+var appUrl = gui.App.manifest.appUrl;
+var newWin = null,
+currentWin = null,
+isConnected = true,
+reloadTimer = null,
+isLoadWindowOpen=false,
+lastNetState = false;
+
+gui.App.clearCache(); // 清除缓存
+
+function handle(cb) {
+    
+    currentWin = gui.Window.get();
+    currentWin.on("close",function(){
+        //newWin.close(true);
+        currentWin.close(true);
+    });
+    
+    startLoadApp(cb);
+
+    reloadTimer = setInterval(function(){
+        startLoadApp(cb);
+    },20*1000);
+
+    global.isLoading = true;
+    global.currentWin = currentWin;    
+}
+
+
+//让加载页窗口跳转到newWin后台打开的项目首页（可直接复用缓存到浏览器中的项目首页资源）
+function loadContentInCurrentWindow(currentWin, appUrl) {
+    currentWin.removeAllListeners("close");
+    currentWin.window.location.href = appUrl;
+}
+
+function checkNetworkConnected(cb){
+    var timeStamp = '?t=' + new Date().getTime();
+
+    var indexHtml = "http://hulaquan.mobi/nw/network",indexHtmlWithTimeStamp = indexHtml+timeStamp;
+    var request = require('request');
+    
+    request(indexHtmlWithTimeStamp,function(err,res,body){
+        if(err){
+            return cb(err);
+        }
+        //console.log(res);
+        if(res.statusCode === 200){
+            return cb(null,true);
+        }
+
+        return cb(null,false);
+    });    
+}
+
+function reloadAppRes(){
+    if(!isLoadWindowOpen){
+        newWin = gui.Window.open(appUrl, {
+            "show": false
+        });
+        currentWin.on("close",function(){
+            newWin.close(true);
+            currentWin.close(true);
+        });
+        newWin.once("loaded", function () {
+            newWin.close(true);
+            loadContentInCurrentWindow(currentWin, appUrl);   
+        });
+        isLoadWindowOpen = true;
+    }else{
+        newWin.window.location.href = appUrl;
+    }
+    
+}
+
+function startLoadApp(cb){
+    checkNetworkConnected(function(err,res){
+        if(err) {
+            isConnected = false;
+            lastNetState = false;
+            cb&&cb();
+        }
+        if(res){
+            isConnected = true;
+            if(!lastNetState){
+                reloadAppRes();
+            }
+            lastNetState = true;
+            cb&&cb();
+        }else{
+            isConnected = false;
+            lastNetState = false;
+            cb&&cb();
+        }
+    });
+}
+
+/*function initApp(){
+    var tray = new gui.Tray({
+                icon: './res/tray@2x.png'
+            });
+    global.tray = tray;
+}*/
+
+//initApp();
+//handle();
